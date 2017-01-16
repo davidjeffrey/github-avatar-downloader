fbabc6af201e2b492dc2e6ebe6b06f412c7e05f9
David Jeffrey <dsejeffrey@gmail.com>
2017-01-16 22:04:36 +0000

    generating url for request

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,26 @@
-var request = require('request');
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+var request = require('request');
+
+var GITHUB_USER = "davidjeffrey";
+var GITHUB_TOKEN = "3bfe02dd19ea8e5ad124b7ea94cc3c19a2af412c";
+
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+console.log(requestURL)
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
+
+
+
